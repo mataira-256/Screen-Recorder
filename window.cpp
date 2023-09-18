26b84f66ab9ea57062ece8cfc44d362518c99ae8
mataira-256 <128944427+mataira-256@users.noreply.github.com>
2023-09-19 01:53:51 +0900

    [add] 設定ウィンドウ
    設定ボタンが押されたら設定ウィンドウが開くようになった

--- window.cpp
+++ window.cpp
@@ -1,71 +1,26 @@
 #include <windows.h>
 
-/* プロシージャのプロトタイプ宣言 ? */
-LRESULT CALLBACK WndProc(
-    HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+/* プロトタイプ宣言 */
+LRESULT CALLBACK MainWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK SettingsWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+void CreateMainWindow();
+void CreateSettingsWindow();
+
+/* グローバル変数等々 */
+HINSTANCE hInstance;
+TCHAR class_main[] = TEXT("MainWindow");
+TCHAR class_settings[] = TEXT("SettingsWindow");
+HWND hwnd_main;
+HWND hwnd_settings;
+
+#define ID_BTN_SETTINGS 1
 
 /* WinMain(WINAPIのmainみたいなもの)の宣言 */
-int WINAPI WinMain(
-    HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    LPSTR lpszCmdLine, int nCmdShow)
-{
-    /* 変数だか構造体だかの宣言 */
-    TCHAR szAppName[] = TEXT("TestApp");
-    WNDCLASS wc_main;
-    HWND hwnd_main;
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow) {
+    hInstance = hInst;
     MSG msg;
 
-    HWND hwnd_button_rec;
-
-    #define CHILD_ID_REC 1;
-
-    /* wc(ウィンドウクラス)の属性を設定*/
-    wc_main.style = CS_HREDRAW | CS_VREDRAW;
-    wc_main.lpfnWndProc = WndProc;
-    wc_main.cbClsExtra = 0;
-    wc_main.cbWndExtra = 0;
-    wc_main.hInstance = hInstance;
-    wc_main.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    wc_main.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc_main.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wc_main.lpszMenuName = NULL;
-    wc_main.lpszClassName = szAppName;
-    
-    /* wcを登録(windowsに認識させる) */
-    if (!RegisterClass(&wc_main)) return 0;
-
-    /* hwnd(ウィンドウハンドル(識別子))の作成 */
-    hwnd_main = CreateWindow(
-        szAppName, TEXT("Screen-Recorder"),
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT,
-        CW_USEDEFAULT, CW_USEDEFAULT,
-        NULL, NULL,
-        hInstance, NULL);
-
-    if (!hwnd_main) return 0;
-
-    ShowWindow(hwnd_main, nCmdShow);
-    UpdateWindow(hwnd_main);     // ウィンドウを再描画(何のためかは分からん)
-
-    hwnd_button_rec = CreateWindow(
-        "button_rec",
-        "録画ボタン",
-        WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
-        100,
-        100,
-        100,
-        50,
-        hwnd_main,
-        (HMENU)CHILD_ID_REC,
-        hInstance,
-        NULL);
-
-
-
-
-
-
+    CreateMainWindow();
 
     /* メッセージループ(イベントとかメッセージを受け取って、処理してくれるとこに送るのと、ウィンドウの維持) */
     while (GetMessage(&msg, NULL, 0, 0) > 0) {
@@ -76,15 +31,124 @@ int WINAPI WinMain(
     return msg.wParam;
 }
 
-/* WndProc(ウィンドウプロシージャ(受け取ったメッセージによって処理をする))の定義 */
-LRESULT CALLBACK WndProc(
-    HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    switch (uMsg) {             // uMsg(受け取ったメッセージが入ってる)が
-    case WM_DESTROY:            // WM_DESTROY(ウィンドウが破棄されたときに発生するメッセージ)なら
-        PostQuitMessage(0);     // 終了コードを0にする
+/* メインウィンドウ系 */
+void CreateMainWindow() {
+    WNDCLASS wc_main;
+    HWND hwnd_btn_settings;
+
+    /* wc_main(ウィンドウクラス)の属性を設定*/
+    wc_main.style = CS_HREDRAW | CS_VREDRAW;
+    wc_main.lpfnWndProc = MainWndProc;
+    wc_main.cbClsExtra = 0;
+    wc_main.cbWndExtra = 0;
+    wc_main.hInstance = hInstance;
+    wc_main.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc_main.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc_main.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc_main.lpszMenuName = NULL;
+    wc_main.lpszClassName = class_main;
+
+    /* wc_mainを登録(windowsに認識させる) */
+    RegisterClass(&wc_main);
+
+    /* hwnd_main(ウィンドウハンドル(識別子))の作成 */
+    hwnd_main = CreateWindow(
+        class_main,
+        TEXT("Screen-Recorder"),
+        WS_OVERLAPPED | WS_SYSMENU,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+    /* 録画ボタンを作る */
+    hwnd_btn_settings = CreateWindow(
+        TEXT("button"),
+        TEXT("設定"),
+        WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+        100,
+        100,
+        100,
+        50,
+        hwnd_main,
+        (HMENU)ID_BTN_SETTINGS,
+        hInstance,
+        NULL);
+
+    ShowWindow(hwnd_main, SW_NORMAL);
+    UpdateWindow(hwnd_main);
+}
+
+/* 設定用ウィンドウ系 */
+void CreateSettingsWindow() {
+    WNDCLASS wc_settings;
+
+    /* wc_settings(ウィンドウクラス)の属性を設定*/
+    wc_settings.style = CS_HREDRAW | CS_VREDRAW;
+    wc_settings.lpfnWndProc = SettingsWndProc;
+    wc_settings.cbClsExtra = 0;
+    wc_settings.cbWndExtra = 0;
+    wc_settings.hInstance = hInstance;
+    wc_settings.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc_settings.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc_settings.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc_settings.lpszMenuName = NULL;
+    wc_settings.lpszClassName = class_settings;
+
+    /* wc_settingsを登録(windowsに認識させる) */
+    RegisterClass(&wc_settings);
+
+    /* hwnd_settings(ウィンドウハンドル(識別子))の作成 */
+    hwnd_settings = CreateWindow(
+        class_settings,
+        TEXT("設定"),
+        WS_POPUPWINDOW | WS_CAPTION,
+        200,
+        200,
+        200,
+        200,
+        hwnd_main,
+        NULL,
+        hInstance,
+        NULL);
+
+    ShowWindow(hwnd_settings, SW_SHOW);
+    UpdateWindow(hwnd_settings);
+
+    EnableWindow(hwnd_main, FALSE);     // メインウィンドウを使用不可に
+
+}
+
+/* MainWndProc(ウィンドウプロシージャ(受け取ったメッセージによって処理をする))の定義 */
+LRESULT CALLBACK MainWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) { // uMsg(受け取ったメッセージが入ってる)が
+    case WM_DESTROY: // WM_DESTROY(ウィンドウが破棄されたときに発生するメッセージ)なら
+        PostQuitMessage(0); // 終了コードを0にする
         return 0;
+    case WM_COMMAND: // WM_COMMAND(何らかのボタンが押されたときに発生するメッセージ)なら
+        switch LOWORD(wParam) {
+        case ID_BTN_SETTINGS:
+            CreateSettingsWindow();
+            break;
+        }
     }
 
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);       // 特別指定したやつ以外は既定のプロシージャにぶん投げる
+    return DefWindowProc(hwnd, uMsg, wParam, lParam); // 特別指定したやつ以外は既定のプロシージャにぶん投げる
+}
+
+/* SettingsWndProc(ウィンドウプロシージャ(受け取ったメッセージによって処理をする))の定義 */
+LRESULT CALLBACK SettingsWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) { // uMsg(受け取ったメッセージが入ってる)が
+    case WM_DESTROY: // WM_DESTROY(ウィンドウが破棄されたときに発生するメッセージ)なら
+        return 0;
+    case WM_CLOSE:
+        EnableWindow(hwnd_main, TRUE);
+        DestroyWindow(hwnd);
+    }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam); // 特別指定したやつ以外は既定のプロシージャにぶん投げる
 }
