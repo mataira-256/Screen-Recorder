1ca8dcf2cf0d3a4b3d3f6ffdd5ece019d0dca655
mataira-256 <128944427+mataira-256@users.noreply.github.com>
2023-11-01 14:55:43 +0900

    [apdate] 何やったかわからん
    ここでどんな変化合ったのか分らないので、将来の僕頑張れ

--- recorder.hpp
+++ recorder.hpp
@@ -1,4 +1,9 @@
 #pragma once
 #include <Windows.h>
+extern "C" {
+#include <libavformat/avformat.h>
+#include <libavutil/opt.h>
+#include <libavcodec/avcodec.h>
+}
 
-void Recorder();
+int Recorder();
--- window.cpp
+++ window.cpp
@@ -32,6 +32,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, 
 void CreateMainWindow() {
     WNDCLASS wc_main;
     HWND hwnd_btn_settings;
+    HWND hwnd_btn_recording;
     HWND hwnd_lb_window;
     HWND hwnd_lb_sound;
 
@@ -64,17 +65,31 @@ void CreateMainWindow() {
         hInstance,
         NULL);
 
-    /* 録画ボタンを作る */
+    /* 設定ボタンを作る */
     hwnd_btn_settings = CreateWindow(
         TEXT("button"),
         TEXT("settings"),
+        WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+        400,
+        350,
+        100,
+        50,
+        hwnd_main,
+        (HMENU)ID_BTN_SETTINGS,
+        hInstance,
+        NULL);
+
+    /* 録画ボタンを作る */
+    hwnd_btn_recording = CreateWindow(
+        TEXT("button"),
+        TEXT("recording"),
         WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
         400,
         400,
         100,
         50,
         hwnd_main,
-        (HMENU)ID_BTN_SETTINGS,
+        (HMENU)ID_BTN_RECORDING,
         hInstance,
         NULL);
 
