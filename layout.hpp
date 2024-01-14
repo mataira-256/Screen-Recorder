68bf6196f098e84fd09145e9ad1fd6f258b1ed1f
mataira-256 <128944427+mataira-256@users.noreply.github.com>
2024-01-15 04:13:34 +0900

    [update] GUIの調整
    GUIを定数を用いてきれいにした

---
+++ layout.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#define MAGIC_HEIGHT 32
+#define MAGIC_WIDTH 15
+
+#define BTN_HEIGHT 25
+#define BTN_WIDTH 100
+#define BOX_HEIGHT 25
+#define CB_HEIGHT 200
+#define BOX_WIDHT 200
+#define MARGIN 20
